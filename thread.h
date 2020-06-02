37ee9cc9d7c97b8798204e19b30309d4a241d5cf
agosvasquez <maria.vasquez@despegar.com>
2020-06-02 14:58:50 -0300

    fix issue #4

--- thread.h
+++ thread.h
@@ -10,6 +10,8 @@ public:
 	void join();
 	virtual void run() = 0;
 	virtual ~Thread();
+	Thread(const Thread&) = delete;
+	Thread& operator=(const Thread&) = delete;
 private:
 	std::thread thread;
 };
